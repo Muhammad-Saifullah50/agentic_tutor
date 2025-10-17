8ea7372eac7ee470125ddd17f8b7d4e31258b125
Muhammad Saifullah <saifullahm2005@gmail.com>
2025-10-17 20:45:28 +0500

    correcetd prisma user null error

--- frontend/src/actions/lesson.actions.ts
+++ frontend/src/actions/lesson.actions.ts
@@ -1,12 +1,30 @@
 'use server'
 
+import { db } from "../lib/prisma"
 import { StudyMode } from "../types"
+import { getCurrentUserfromDb } from "./user.actions"
 
 export const createLesson = async (topic: string, clerkId: string, mode: StudyMode) => {
 
     try {
-        
+
+        const user = await getCurrentUserfromDb();
+
+        const lesson = await db.lesson.create({
+            data: {
+                topic,
+                mode,
+                clerkId: clerkId,
+                flashcards: [],
+                questions: [],
+                userId: user.id
+            }
+        });
+
+        return lesson
     } catch (error) {
         
     }
 }
+
+
